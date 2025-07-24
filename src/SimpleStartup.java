ae57e0943f7a6f9221aca004b7e927b6471f0d1f
antonkulbakov <antonkulbakov@antons-mac-mini.fritz.box>
2025-07-24 11:13:22 +0200

    making tests

---
+++ src/SimpleStartup.java
@@ -0,0 +1,15 @@
+
+public class SimpleStartup {
+	int[] locationCell;
+	int numOfHits=0;
+	
+	String checkYourself(int cell) {
+		return "";
+	}
+	
+	int setLocationCells(int[] guess) {
+		return 0;
+	}
+	
+	
+}
