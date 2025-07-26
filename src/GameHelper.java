6e0c0a36b145cdb233eb8eeed74461ceae25f389
antonkulbakov <antonkulbakov@antons-mac-mini.fritz.box>
2025-07-26 14:49:25 +0200

    Rewrite Startup and write StartupBurst classes

--- src/GameHelper.java
+++ src/GameHelper.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class GameHelper {
 	
 	//TODO make a class
-	int getUserInput(String promt) {
+	String getUserInput(String promt) {
 		System.out.print(promt+": ");
 		Scanner scanner=new Scanner(System.in);
 		return scanner.nextInt();
+	}
+
+	public ArrayList<String> placeStartup(int i) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
